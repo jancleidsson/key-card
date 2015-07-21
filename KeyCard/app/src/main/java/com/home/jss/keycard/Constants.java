5c0fedf706588143952a60a817e39346ed6eff3d
Jancleidsson Soares <jss@cesar.org.br>
2015-07-21 17:56:23 -0300

    Adding xml to some screens

---
+++ KeyCard/app/src/main/java/com/home/jss/keycard/Constants.java
@@ -0,0 +1,9 @@
+package com.home.jss.keycard;
+
+/**
+ * Created by jss on 7/21/15.
+ */
+public class Constants {
+
+    public final static String MAIN_KEY = "MainKey";
+}
--- KeyCard/app/src/main/java/com/home/jss/keycard/MainActivity.java
+++ KeyCard/app/src/main/java/com/home/jss/keycard/MainActivity.java
@@ -1,13 +1,39 @@
 package com.home.jss.keycard;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
+
+    private HashMap<Integer, Integer> mPasswordsMap;
+    private SharedPreferences mSharedPreferences;
+    private boolean mForceChangePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeParameters();
+        checkMainPassword();
     }
+
+    private void initializeParameters()
+    {
+        mPasswordsMap =  new HashMap<>();
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    private void checkMainPassword(){
+
+      if(mSharedPreferences.contains(Constants.MAIN_KEY) || !mForceChangePassword) {
+          Intent intent = new Intent();
+          startActivity(new Intent(this, SearchPasswordActivity.class));
+      };
+    }
+
 }
